e4819db72aa9b569bb49fc17763bf8e6a6b91a20
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-03-22 22:51:08 +0100

    small updates

--- src/components/sections/benefits/Benefits.tsx
+++ src/components/sections/benefits/Benefits.tsx
@@ -10,7 +10,7 @@ const Benefits = () => {
             <Box title={"Benefits"} text={"$WSDM is a crypto utility token which provides access to the best in-class motivational wisdoms and quotes from the world elite performers!"} />
         </div>
         <div className={`${styles.right}`}>
-            <Image style={{filter:'invert(100%)'}} src={'/benefits.png'} width={400} height={400} alt='benefits'/>
+            <Image src={'/benefits.png'} width={400} height={400} alt='benefits'/>
         </div>
     </section>
   )
