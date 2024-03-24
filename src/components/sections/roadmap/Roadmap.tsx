4ad250a00de3b215ab4bc3fccbba6ce97f8814e2
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-03-24 22:45:03 +0100

    build website

--- src/components/sections/roadmap/Roadmap.tsx
+++ src/components/sections/roadmap/Roadmap.tsx
@@ -5,7 +5,7 @@ import RoadmapLine from '@/components/roadmap-line/RoadmapLine'
 const checkpoints = [
  { roadmapCheckpoint: 'Create Telegram', isDone: true },
  { roadmapCheckpoint: 'Create Twitter', isDone: true },
- { roadmapCheckpoint: 'Build Website', isDone: false },
+ { roadmapCheckpoint: 'Build Website', isDone: true },
  { roadmapCheckpoint: 'Launch $WSDM Token', isDone: false },
  { roadmapCheckpoint: 'Add Liquidity', isDone: false },
  { roadmapCheckpoint: 'Coingecko Listing', isDone: false },
