eb6779963c34ff3ef0602e32944e4140ac6c824d
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-04-04 18:32:46 +0200

    partikles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import TopBar from "@/components/topbar/Topbar";
+import ParticlesEffect from "./particles";
 
 export const metadata: Metadata = {
   title: "WSDM - Wisdomia",
@@ -8,14 +9,12 @@ export const metadata: Metadata = {
 };
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) 
+{
   return (
     <html lang="en">
       <body className={`mainLayout`}>
+        <ParticlesEffect/>
         <TopBar /> 
         {children}
       </body>
