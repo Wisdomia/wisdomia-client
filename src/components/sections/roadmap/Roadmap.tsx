d09039708e66cb74db6d2d1b6e522b93d7bd1ae8
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-04-06 11:35:03 +0200

    fix stuff

--- src/components/sections/roadmap/Roadmap.tsx
+++ src/components/sections/roadmap/Roadmap.tsx
@@ -8,14 +8,13 @@ const checkpoints = [
  { roadmapCheckpoint: 'Create Telegram', isDone: true },
  { roadmapCheckpoint: 'Grow Community', isDone: false },
  { roadmapCheckpoint: 'Launch $WSDM Token', isDone: false },
- { roadmapCheckpoint: 'Open Source Github', isDone: false },
  { roadmapCheckpoint: 'Raydium LP', isDone: false },
  { roadmapCheckpoint: 'Coingecko Listing', isDone: false },
- { roadmapCheckpoint: 'FAQ & Support Desk', isDone: false },
  { roadmapCheckpoint: 'Dex Screener Listing', isDone: false },
  { roadmapCheckpoint: 'Dex Tools Listing', isDone: false },
  { roadmapCheckpoint: 'CoinMarketCap Listing', isDone: false },
  { roadmapCheckpoint: 'CEX Listing (MEXC)', isDone: false },
+ { roadmapCheckpoint: 'Open Source Github', isDone: false },
  { roadmapCheckpoint: 'Launch Discord', isDone: false },
  { roadmapCheckpoint: 'Wisdomia Platform', isDone: false }
 ]
